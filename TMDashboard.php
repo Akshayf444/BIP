96775126cde3ec88ae54e0e0f5725293a0a837c3
Akshayf444 <mohite.akshay118@gmail.com>
2015-12-08 18:15:16 +0530

    Changes BM dashboard and activity query

--- TMDashboard.php
+++ TMDashboard.php
@@ -239,7 +239,7 @@ require_once './header.php';
                 <div u="slides" style="cursor: move; text-align:center; position: absolute; left: 0px; top: 0px; width: 600px; height: 300px; overflow: hidden;">
                     <div>
                         <img u="image" src="Images/darkback.jpg" />
-                        <div id="dv_top"  style="position: absolute; top: 10px;width: 100%; height: 20px; font-size: 25px; font-weight:500; color: #fff; line-height: 30px;">Most No. Of Rotahaler Changed</div>
+                        <div id="dv_top"  style="position: absolute; top: 10px;width: 100%; height: 20px; font-size: 25px; font-weight:500; color: #fff; line-height: 30px;">Most No. Of BIP Device Check Camp</div>
                         <?php
                         $condition = array('GROUP BY TM_Emp_Id ORDER BY device_check DESC LIMIT 1');
                         $toppers = Activity::TMActivity($condition);
@@ -247,7 +247,7 @@ require_once './header.php';
                             $topper = array_shift($topper);
                         }
                         ?>
-                        <div id="dv_tm1name"  u="caption" t="FLTTR|R" style="position: absolute; left:0px; top: 70px; width: 100%; height: 20px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->TM_Name) ? $topper->BM_Name : 'NA'; ?></div>
+                        <div id="dv_tm1name"  u="caption" t="FLTTR|R" style="position: absolute; left:0px; top: 70px; width: 100%; height: 20px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->TM_Name) ? $topper->TM_Name : 'NA'; ?></div>
                         <br />
                         <div id="dv_tm1Region"  u="caption" t="FLTTR|R" style="position: absolute; left:0px; top: 120px; width: 100%;height: 10px; font-size: 36px; color: #fff; line-height: 40px;"><?php echo isset($topper->device_check) ? $topper->device_check : '-'; ?></div>
                         <br />
@@ -262,8 +262,8 @@ require_once './header.php';
                         }
                         ?>
                         <img u="image" src="Images/darkback.jpg" />
-                        <div id="Div1"  style="position: absolute; top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of Rotahaler Changed</div>
-                        <div id="dv_tm2name"  u="caption" t="ZMF|10" style="position: absolute;left:0px; top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->BM_Name) ? $topper->BM_Name : 'NA'; ?></div>
+                        <div id="Div1"  style="position: absolute; top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of BIP Paramedic Meet</div>
+                        <div id="dv_tm2name"  u="caption" t="ZMF|10" style="position: absolute;left:0px; top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->TM_Name) ? $topper->TM_Name : 'NA'; ?></div>
                         <br />
                         <div id="dv_tm2Region"  u="caption" t="ZMF|10" style="position: absolute; left:0px;top: 120px; width: 100%; height: 40px; font-size: 36px; color: #fff; line-height: 40px;"><?php echo isset($topper->paramedic) ? $topper->paramedic : '-'; ?></div>
                         <br />
@@ -278,8 +278,8 @@ require_once './header.php';
                         }
                         ?>
                         <img u="image" src="Images/darkback.jpg" />
-                        <div id="Div2"  style="position: absolute;  top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of Rotahaler Changed</div>
-                        <div id="dv_tm3name"  u="caption" t="RTT|10" style="position: absolute;left:0px;  top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->BM_Name) ? $topper->BM_Name : 'NA'; ?></div>
+                        <div id="Div2"  style="position: absolute;  top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of BIP Chemist Meet</div>
+                        <div id="dv_tm3name"  u="caption" t="RTT|10" style="position: absolute;left:0px;  top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->TM_Name) ? $topper->TM_Name : 'NA'; ?></div>
                         <br />
                         <div id="dv_tm3Region"  u="caption" t="RTT|10" style="position: absolute;left:0px; top: 120px; width: 100%; height: 40px; font-size: 36px; color: #fff; line-height: 40px;"><?php echo isset($topper->chemist_meet) ? $topper->chemist_meet : '-'; ?></div>
                         <br />
@@ -294,8 +294,8 @@ require_once './header.php';
                         }
                         ?>
                         <img u="image" src="Images/darkback.jpg" />
-                        <div id="Div2"  style="position: absolute;  top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of Rotahaler Changed</div>
-                        <div id="dv_tm3name"  u="caption" t="RTT|10" style="position: absolute;left:0px;  top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->BM_Name) ? $topper->BM_Name : 'NA'; ?></div>
+                        <div id="Div2"  style="position: absolute;  top: 10px;width: 100%; height: 20px; font-size: 23px; color: #fff; line-height: 30px;">Most No. Of Visibility At Clinics</div>
+                        <div id="dv_tm3name"  u="caption" t="RTT|10" style="position: absolute;left:0px;  top: 70px; width: 100%; height: 40px; font-size: 43px; color: #fff; line-height: 40px;"><?php echo isset($topper->TM_Name) ? $topper->TM_Name : 'NA'; ?></div>
                         <br />
                         <div id="dv_tm3Region"  u="caption" t="RTT|10" style="position: absolute;left:0px; top: 120px; width: 100%; height: 40px; font-size: 36px; color: #fff; line-height: 40px;"><?php echo isset($topper->visibility) ? $topper->visibility : '-'; ?></div>
                         <br />
--- includes/Activity.php
+++ includes/Activity.php
@@ -10,15 +10,16 @@ class Activity extends Table {
     }
 
     public static function BMActivity($conditions = array()) {
-        $sql = "SELECT SUM(launch) AS launch,SUM(device_check) AS device_check,SUM(paramedic) AS paramedic,SUM(chemist_meet) AS chemist_meet,SUM(visibility) AS visibility,SUM(revolizer) AS revolizer,SUM(zvt) AS zvt,SUM(rotahaler) AS rotahaler,act_id FROM BM_Activity ";
+        $sql = "SELECT SUM(launch) AS launch,SUM(device_check) AS device_check,SUM(paramedic) AS paramedic,SUM(chemist_meet) AS chemist_meet,SUM(visibility) AS visibility,SUM(revolizer) AS revolizer,SUM(zvt) AS zvt,SUM(rotahaler) AS rotahaler,act_id,bmp.Zone,bmp.Region,bmp.Territory,bmp.TM_Name,bmp.BM_Name FROM BM_Activity ba INNER JOIN bip_manpower bmp ON bmp.smswayid = ba.smswayid  ";
         if (!empty($conditions)) {
             $sql .= join(" ", $conditions);
         }
+        //echo $sql;
         return Query::executeQuery($sql);
     }
 
     public static function TMActivity($conditions = array()) {
-        $sql = "SELECT SUM(launch) AS launch,SUM(device_check) AS device_check,SUM(paramedic) AS paramedic,SUM(chemist_meet) AS chemist_meet,SUM(visibility) AS visibility,SUM(revolizer) AS revolizer,SUM(zvt) AS zvt,SUM(rotahaler) AS rotahaler,act_id FROM TM_Activity  ";
+        $sql = "SELECT SUM(launch) AS launch,SUM(device_check) AS device_check,SUM(paramedic) AS paramedic,SUM(chemist_meet) AS chemist_meet,SUM(visibility) AS visibility,SUM(revolizer) AS revolizer,SUM(zvt) AS zvt,SUM(rotahaler) AS rotahaler,act_id,bmp.Zone,bmp.Region,bmp.Territory,bmp.TM_Name,bmp.BM_Name FROM TM_Activity ta INNER JOIN bip_manpower bmp ON bmp.smswayid = ta.smswayid  ";
         if (!empty($conditions)) {
             $sql .= join(" ", $conditions);
         }
