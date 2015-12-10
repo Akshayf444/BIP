3c08e5bde5014c90d55997b9f5c9945317d3de16
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-10 11:11:00 +0530

    bug fixes

--- SM_View.php
+++ SM_View.php
@@ -9,7 +9,7 @@ require_once './header.php';
 if (isset($_GET['report'])) {
     $report = $_GET['report'];
     if ($report == 'TM Report') {
-       
+
         $check = Activity::TMActivity_Report($_SESSION['smemp']);
     } else {
         $check = Activity::BMActivity_Report($_SESSION['smemp']);
@@ -25,12 +25,12 @@ if (isset($_GET['report'])) {
             <form action="SM_View.php" method="GET">
                 <select class="form-control" name="report">
                     <option value="TM Report" <?php
-                    if (isset($report) && $report== 'TM Report') {
+                    if (isset($report) && $report == 'TM Report') {
                         echo"selected";
                     }
                     ?>>TM Report</option>
                     <option value="BM Report" <?php
-                    if (isset($report) && $report== 'BM Report') {
+                    if (isset($report) && $report == 'BM Report') {
                         echo"selected";
                     }
                     ?>>BM Report</option>
@@ -45,6 +45,13 @@ if (isset($_GET['report'])) {
         <?php if (!empty($check)) { ?>
             <table class="table table-bordered table-stripped">
                 <tr>
+                    <th><?php
+                        if (isset($report) && $report == 'BM Report') {
+                            echo "BM Name";
+                        } else {
+                            echo "TM Name";
+                        }
+                        ?></th>
                     <th>BIP Device Check Camp</th>
                     <th>BIP Paramedic Meet</th>
                     <th>BIP Chemist Meet</th>
@@ -52,6 +59,13 @@ if (isset($_GET['report'])) {
                 </tr>
 
                 <tr>
+                    <td><?php
+                        if (isset($report) && $report == 'BM Report') {
+                            echo $check->BM_Name;
+                        } else {
+                            echo $check->TM_Name;
+                        }
+                        ?></td>
                     <td><?php echo $check->device_check; ?></td>
                     <td><?php echo $check->paramedic; ?></td>
                     <td><?php echo $check->chemist_meet; ?></td>
